ace01999d9c109f084d690f2ffd4b519a5c80ffa
Sandcobra <unit202301@chris-ludwigs-MacBook-Pro.local>
2018-07-14 13:13:25 -0400

    done

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "8WetoSupGVany3NiUahlflgwg"
+consumer_secret = "Bx0gcdxbtJnm0EyxbDjnPzMyVOVUZyyO7doN7hkuDsISe8H92g"
+access_token = "29990724-DCUWQmDL5pkxUGursf8Uyh0TuJo46OXGaI4DsOe1q"
+access_token_secret = "tIvuIGL0QxNe6UTy0xDIIV0pYfdmU31kmDeulbcSUCduM"
